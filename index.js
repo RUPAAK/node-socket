397b97dd190ad2db02723b7c38ddd8333985c26b
rupaak <rupakt525@gmail.com>
2022-11-15 20:29:06 +0545

    initialize socket

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("send_message", (msg) => {
+    console.log("message: " + msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnect");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
